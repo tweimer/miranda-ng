be747bf31cbcbc5a51a62471edfc4e1832dad6bc
Tobias Weimer <wishmaster51@gmail.com>
2024-09-10 11:49:48 +0200

    Update cmstub.cpp
    Compile fix

--- build/cmplugin/cmstub.cpp
+++ build/cmplugin/cmstub.cpp
@@ -6,7 +6,7 @@ struct CMPlugin : public PLUGIN<CMPlugin>
 {
 };
 
-static BOOL WINAPI RawDllMain(HINSTANCE hInstance, DWORD, LPVOID)
+static BOOL WINAPI RawDllMain(HINSTANCE hInstance, uint32_t, LPVOID)
 {
 	g_plugin.setInst(hInstance);
 	return TRUE;
